da96ed40e2deb102cb666b27a2c45bce338e7a24
Youngjo <youngjk50127@gmail.com>
2019-08-05 14:41:36 +0900

    영양성분

---
+++ client/src/pages/Nutrition.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Progress } from 'reactstrap';
+import './Nutrition.css';
+
+const Progress = (props) => {
+  return (
+    <div>
+      <div className="text-center">1 of 5</div>
+      <Progress value="1" max="100" />
+      <div className="text-center">50 of 135</div>
+      <Progress value={50} max="100" />
+      <div className="text-center">75 of 111</div>
+      <Progress value={75} max={111} />
+      <div className="text-center">463 of 500</div>
+      <Progress value="463" max={500} />
+
+
+
+      </div>
+  );
+};
+
+export default Example;
