d2a1375c337bc68d4154a9ecd6595e0560c47977
Youngjo <youngjk50127@gmail.com>
2019-08-05 11:10:43 +0900

    190805 login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import './Login.css';
 
 const Login = () => {
     return (
-        <div>
+        <div border ="1px soild">
+            <Form>
             <h2>Login</h2>
+            <br />
+            <FormGroup>
+                <Label className="id">ID/Email</Label>
+                <Input type="userid" name="userid" placeholder="ID/Email" />
+            </FormGroup>
+
+            <FormGroup>
+                <Label className="pw">Password</Label>
+                <Input type="password" name="password" placeholder="Password" />
+            </FormGroup>
+
+            <FormGroup check>
+          <Label check>
+            <Input type="checkbox" />{' '}
+            Remember me
+          </Label>
+
+          <Label className="forgotpw">
+             <i>Forgot password?</i>
+          </Label>
+        </FormGroup>
+
+        <br />
+        <Button color="primary">Submit</Button>
+        <br />
+
+
+        <Label className="connectsignup">
+            Don't have an account? SIGN UP!
+          </Label>
+
+
+        </Form>
         </div>
+      
     );
 };
 
