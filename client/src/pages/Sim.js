0da1b0540c5b454eaba14d9d7d001ebc77e97962
yeeun0855 <e11en1213@naver.com>
2019-08-07 10:43:19 +0900

    유사상품 전
    ㅜ푸ㅠㅜㅠㅜㅠㅠㅍㅍ

--- client/src/pages/Sim.js
+++ client/src/pages/Sim.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Carousel from "react-multi-carousel";
-import {Card, Button } from 'reactstrap';
+import TMPCard from './tmpCard';
 import "react-multi-carousel/lib/styles.css";
+
 
 const responsive = {
     desktop: {
@@ -22,6 +23,7 @@ const responsive = {
 };
 
 class Sim extends Component {
+
     render() {
         return (
             <div>
@@ -69,62 +71,15 @@ class Sim extends Component {
                     sliderClass=""
                     slidesToSlide={1}
                     swipeable
-                    >
+                >
 
+                    <TMPCard data={this.props.data} />
 
-                    <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
-
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
                     {/* <WithStyles
                         description="React Carousel with Server Side Rendering Support – Part 1"
                         headline="w3js.com - web front-end studio"
                         image="https://images.unsplash.com/photo-1549989476-69a92fa57c36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-                    />
-                    <WithStyles
-                        description="Appending currency sign to a purchase form in your e-commerce site using plain JavaScript."
-                        headline="w3js.com - web front-end studio"
-                        image="https://images.unsplash.com/photo-1549396535-c11d5c55b9df?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60"
-                    />
-                    <WithStyles
-                        description="React Carousel with Server Side Rendering Support – Part 1"
-                        headline="w3js.com - web front-end studio"
-                        image="https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-                    />
-                    <WithStyles
-                        description="Appending currency sign to a purchase form in your e-commerce site using plain JavaScript."
-                        headline="w3js.com - web front-end studio"
-                        image="https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-                    />
-                    <WithStyles
-                        description="Fixing CSS load order/style.chunk.css incorrect in Nextjs"
-                        headline="w3js.com - web front-end studio"
-                        image="https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-                    />
-                    <WithStyles
-                        description="React Carousel with Server Side Rendering Support – Part 1"
-                        headline="w3js.com - web front-end studio"
-                        image="https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
-                    /> */}
+                    />*/}
                 </Carousel>
             </div>
         );
---
+++ client/src/pages/tmpCard.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Card from 'react-bootstrap/Card'
+import { Button } from 'reactstrap';
+import './tmpCard.css'
+
+class tmpCard extends Component {
+
+    render() {
+        return (
+            <div>
+                <Card className="inlinecard" style={{ width: '8rem' }}>
+                    <Card.Img variant="top" src={this.props.data.imgUrl} />
+                    <Card.Body>
+                        <Card.Text>{this.props.data.name}</Card.Text>
+                        <Button className="btn1" variant="primary">Go</Button>
+                    </Card.Body>
+                </Card>
+
+                <Card className="inlinecard" style={{ width: '8rem' }}>
+                    <Card.Img variant="top" src={this.props.data.imgUrl} />
+                    <Card.Body>
+                        <Card.Text>{this.props.data.name}</Card.Text>
+                        <Button className="btn1" variant="primary">Go</Button>
+                    </Card.Body>
+                </Card>
+                <Card className="inlinecard" style={{ width: '8rem' }}>
+                    <Card.Img variant="top" src={this.props.data.imgUrl} />
+                    <Card.Body>
+                        <Card.Text>{this.props.data.name}</Card.Text>
+                        <Button className="btn1" variant="primary">Go</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default tmpCard;
