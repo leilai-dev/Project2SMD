a34c592b22a904c0cf911a0fe16eb3d60e132fc8
leilaiGitKraken <leilailai@hotmail.com>
2019-09-02 12:21:40 +0900

    -

--- schemas/index.js
+++ schemas/index.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 module.exports = () => {
     // 로컬환경 mongodb 연결
     mongoose.connect(process.env.MONGODB_URI || mongoUri, {
-    // heroku 빌드시 mongodb uri 변경 < db유저/패스워드 관리는 어떻게?
-    // mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds157857.mlab.com:57857/heroku_x7r5lscv', {
+        // heroku 빌드시 mongodb uri 변경 < db유저/패스워드 관리는 어떻게?
+        // mongoose.connect('mongodb://<dbuser>:<dbpassword>@ds157857.mlab.com:57857/heroku_x7r5lscv', {
         useNewUrlParser: true
     }, (err) => {
         if (err) {
